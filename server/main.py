598badc8a802d32959444cf2bfd4bf5ff92b2235
Andrew Fogarty <andrew.f.fogarty@gmail.com>
2015-12-18 18:22:13 -0500

    New: Simple API

--- server/main.py
+++ server/main.py
@@ -1,9 +1,26 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+from flask import Flask, render_template, url_for
+from flask.ext.api import FlaskAPI
+from flask.ext.api.decorators import set_renderers
+from flask.ext.api.renderers import HTMLRenderer
+#app = Flask(__name__)
+app = FlaskAPI(__name__)
+
+@app.route("/graph/")
+def bar_graph():
+    return {
+        "bar1": 10,
+        "bar2": 30,
+        "bar3": 60
+    }
 
 @app.route("/")
+@set_renderers(HTMLRenderer)
 def hello():
-    return render_template('index.html')
+    js_files = [
+        url_for("static", filename="js/d3.js")
+    ]
+    return render_template('index.html', js_files=js_files)
 
 if __name__ == "__main__":
+    app.debug = True
     app.run(host='0.0.0.0', port=8000)
