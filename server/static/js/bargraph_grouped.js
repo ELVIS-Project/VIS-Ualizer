0b921ebaf86e3688ed894957e4e64847a1b3bd40
AFFogarty <andrew.f.fogarty@gmail.com>
2016-01-12 16:54:36 -0500

    New: Code structure
    Refs #23

--- server/static/js/bargraph_grouped.js
+++ server/static/js/bargraph_grouped.js
@@ -1,30 +1,106 @@
 
 
-var BarGraph = function(selector, width, height) {
-    this.margin = {top: 20, right: 30, bottom: 30, left: 40};
+var BarGraphGrouped = function(selector, width, height) {
 
-    this.width = width - this.margin.left - this.margin.right;
-    this.height = height - this.margin.left - this.margin.right;
+    var margin = {top: 20, right: 30, bottom: 30, left: 40};
 
-    this.x = d3.scale.ordinal()
-        .rangeRoundBands([0, this.width], .1);
+    function chart(data) {
+        // Get all labels
+        var labels = d3.set();
+        data.forEach(function(group) {
+            group.group_members.forEach(function(bar) {
+                labels.add(bar.label);
+            });
+        });
 
-    this.y = d3.scale.linear()
-        .range([this.height, 0]);
+        var colors = d3.scale.category20().domain(labels.values());
 
-    this.xAxis = d3.svg.axis()
-        .scale(this.x)
+        var maxValue = d3.max(data,
+            function(d) {
+                return d3.max(d.group_members, function(member) {
+                    return member.value;
+                })
+            }
+        );
+
+        // Maximum number of members per group
+        var maxWidth = d3.max(data,
+            function(d) {
+                return d.group_members.length;
+            }
+        );
+
+        // Map x-axis labels
+        chart.x.domain(data.map(function(d) { return d.group_label; }));
+        //chart.x.domain(labels.values());
+        // Map y-axis values
+        chart.y.domain([0, maxValue]);
+
+        chart.svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + chart.height + ")")
+            .call(chart.xAxis);
+
+        chart.svg.append("g")
+            .attr("class", "y axis")
+            .call(chart.yAxis);
+
+        data.forEach(function(group) {
+            console.log("group", group);
+
+            var groupOffset = chart.x(group.group_label);
+            // Get the group labels
+            var groupLabels = d3.map(group.group_members, function(d) { return d.label; }).keys();
+
+            // The ordinal of this group        // The ordinal of this group
+            var o = d3.scale.ordinal()
+                .domain(groupLabels)
+                .rangeBands([0, chart.x.rangeBand()]);
+
+            var groupArea = chart.svg.append("g")
+                .attr("label", group.group_label)
+                .attr("class", "group")
+                .attr("height", chart.height)
+                .attr("width", chart.x.rangeBand())
+                .attr("x", chart.x(group.group_label));
+
+
+            groupArea.selectAll(".group")
+                .data(group.group_members)
+                .enter().append("rect")
+                .attr("class", "bar")
+                .attr("x", function(d) { return groupOffset + o(d.label) })
+                .attr("y", function(d) { return chart.y(d.value); })
+                .attr("fill", function(d) { console.log(d); return colors(d.label) })
+                .attr("height", function(d) { return chart.height - chart.y(d.value); })
+                .attr("width", o.rangeBand());
+        });
+    }
+
+    chart.width = width - margin.left - margin.right;
+    chart.height = height - margin.left - margin.right;
+
+    chart.x = d3.scale.ordinal()
+        .rangeRoundBands([0, chart.width], .1);
+
+    chart.y = d3.scale.linear()
+        .range([chart.height, 0]);
+
+    chart.xAxis = d3.svg.axis()
+        .scale(chart.x)
         .orient("bottom");
 
-    this.yAxis = d3.svg.axis()
-        .scale(this.y)
+    chart.yAxis = d3.svg.axis()
+        .scale(chart.y)
         .orient("left");
 
-    this.chart = d3.select(selector)
-        .attr("width", this.width + this.margin.left + this.margin.right)
-        .attr("height", this.height + this.margin.top + this.margin.bottom)
+    chart.svg = d3.select(selector)
+        .attr("width", chart.width + margin.left + margin.right)
+        .attr("height", chart.height + margin.top + margin.bottom)
         .append("g")
-        .attr("transform", "translate(" + this.margin.left + "," + this.margin.top + ")");
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    return chart;
 };
 
 
@@ -32,76 +108,6 @@ var BarGraph = function(selector, width, height) {
  * Load the bar graph data from the server, and render it if successful.
  */
 d3.json("/graph/grouped/", function(error, data) {
-    var barGraph = new BarGraph(".bar-graph-grouped", 640, 320);
-
-    // Get all labels
-    var labels = d3.set();
-    data.forEach(function(group) {
-        group.group_members.forEach(function(bar) {
-            labels.add(bar.label);
-        });
-    });
-
-    var colors = d3.scale.category20().domain(labels.values());
-
-    var maxValue = d3.max(data,
-        function(d) {
-            return d3.max(d.group_members, function(member) {
-                return member.value;
-            })
-        }
-    );
-
-    // Maximum number of members per group
-    var maxWidth = d3.max(data,
-        function(d) {
-            return d.group_members.length;
-        }
-    );
-
-    // Map x-axis labels
-    barGraph.x.domain(data.map(function(d) { return d.group_label; }));
-    //barGraph.x.domain(labels.values());
-    // Map y-axis values
-    barGraph.y.domain([0, maxValue]);
-
-    barGraph.chart.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + barGraph.height + ")")
-        .call(barGraph.xAxis);
-
-    barGraph.chart.append("g")
-        .attr("class", "y axis")
-        .call(barGraph.yAxis);
-
-    data.forEach(function(group) {
-        console.log("group", group);
-
-        var groupOffset = barGraph.x(group.group_label);
-        // Get the group labels
-        var groupLabels = d3.map(group.group_members, function(d) { return d.label; }).keys();
-
-        // The ordinal of this group        // The ordinal of this group
-        var o = d3.scale.ordinal()
-            .domain(groupLabels)
-            .rangeBands([0, barGraph.x.rangeBand()]);
-
-        var groupArea = barGraph.chart.append("g")
-            .attr("label", group.group_label)
-            .attr("class", "group")
-            .attr("height", barGraph.height)
-            .attr("width", barGraph.x.rangeBand())
-            .attr("x", barGraph.x(group.group_label));
-
-
-        groupArea.selectAll(".group")
-            .data(group.group_members)
-            .enter().append("rect")
-            .attr("class", "bar")
-            .attr("x", function(d) { return groupOffset + o(d.label) })
-            .attr("y", function(d) { return barGraph.y(d.value); })
-            .attr("fill", function(d) { console.log(d); return colors(d.label) })
-            .attr("height", function(d) { return barGraph.height - barGraph.y(d.value); })
-            .attr("width", o.rangeBand());
-    });
+    var barGraphGrouped = new BarGraphGrouped(".bar-graph-grouped", 640, 320);
+    barGraphGrouped(data);
 });
