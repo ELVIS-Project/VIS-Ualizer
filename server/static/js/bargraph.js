13dbef08757a8e846bfdb86ad3cf01fb3c91b670
AFFogarty <andrew.f.fogarty@gmail.com>
2016-01-12 16:32:10 -0500

    New: Proper structure
    Refs #23

--- server/static/js/bargraph.js
+++ server/static/js/bargraph.js
@@ -1,30 +1,61 @@
 
 
 var BarGraph = function(selector, width, height) {
-    this.margin = {top: 20, right: 30, bottom: 30, left: 40};
+    var margin = {top: 20, right: 30, bottom: 30, left: 40};
 
-    this.width = width - this.margin.left - this.margin.right;
-    this.height = height - this.margin.left - this.margin.right;
+    function chart(data) {
+        var labels = d3.map(data, function(d) { return d.label }).keys();
+        var colours = d3.scale.category20().domain(labels);
 
-    this.x = d3.scale.ordinal()
-        .rangeRoundBands([0, this.width], .1);
+        // Map x-axis labels
+        chart.x.domain(data.map(function(d) { return d.label; }));
+        // Map y-axis values
+        chart.y.domain([0, d3.max(data, function(d) { return d.value; })]);
 
-    this.y = d3.scale.linear()
-        .range([this.height, 0]);
+        chart.svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + chart.height + ")")
+            .call(chart.xAxis);
 
-    this.xAxis = d3.svg.axis()
-        .scale(this.x)
+        chart.svg.append("g")
+            .attr("class", "y axis")
+            .call(chart.yAxis);
+
+        chart.svg.selectAll(".bar")
+            .data(data)
+            .enter().append("rect")
+            .attr("class", "bar")
+            .attr("x", function(d) { return chart.x(d.label); })
+            .attr("y", function(d) { return chart.y(d.value); })
+            .attr("fill", function(d) { return colours(d.label) })
+            .attr("height", function(d) { return chart.height - chart.y(d.value); })
+            .attr("width", chart.x.rangeBand());
+    }
+
+    chart.width = width - margin.left - margin.right;
+    chart.height = height - margin.left - margin.right;
+
+    chart.x = d3.scale.ordinal()
+        .rangeRoundBands([0, chart.width], .1);
+
+    chart.y = d3.scale.linear()
+        .range([chart.height, 0]);
+
+    chart.xAxis = d3.svg.axis()
+        .scale(chart.x)
         .orient("bottom");
 
-    this.yAxis = d3.svg.axis()
-        .scale(this.y)
+    chart.yAxis = d3.svg.axis()
+        .scale(chart.y)
         .orient("left");
 
-    this.chart = d3.select(selector)
-        .attr("width", this.width + this.margin.left + this.margin.right)
-        .attr("height", this.height + this.margin.top + this.margin.bottom)
+    chart.svg = d3.select(selector)
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
         .append("g")
-        .attr("transform", "translate(" + this.margin.left + "," + this.margin.top + ")");
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    return chart;
 };
 
 
@@ -33,32 +64,5 @@ var BarGraph = function(selector, width, height) {
  */
 d3.json("/graph/", function(error, data) {
     var barGraph = new BarGraph(".bar-graph", 640, 320);
-
-    var labels = d3.map(data, function(d) { return d.label }).keys();
-
-    var colours = d3.scale.category20().domain(labels);
-
-    // Map x-axis labels
-    barGraph.x.domain(data.map(function(d) { return d.label; }));
-    // Map y-axis values
-    barGraph.y.domain([0, d3.max(data, function(d) { return d.value; })]);
-
-    barGraph.chart.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + barGraph.height + ")")
-        .call(barGraph.xAxis);
-
-    barGraph.chart.append("g")
-        .attr("class", "y axis")
-        .call(barGraph.yAxis);
-
-    barGraph.chart.selectAll(".bar")
-        .data(data)
-        .enter().append("rect")
-        .attr("class", "bar")
-        .attr("x", function(d) { return barGraph.x(d.label); })
-        .attr("y", function(d) { return barGraph.y(d.value); })
-        .attr("fill", function(d) { return colours(d.label) })
-        .attr("height", function(d) { return barGraph.height - barGraph.y(d.value); })
-        .attr("width", barGraph.x.rangeBand());
+    barGraph(data);
 });
