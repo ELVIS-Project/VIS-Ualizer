cd1bb0ac41141820e4d1bd76548fde1d430932d7
AFFogarty <andrew.f.fogarty@gmail.com>
2016-01-12 12:38:34 -0500

    New: Basic Force-Directed Graph
    Refs #11
    
    Based on this: http://bl.ocks.org/mbostock/4600693

---
+++ server/static/js/force_directed_graph.js
@@ -0,0 +1,64 @@
+
+var width = 960,
+    height = 500;
+
+var color = d3.scale.category20();
+
+var force = d3.layout.force()
+    .linkDistance(10)
+    .linkStrength(2)
+    .size([width, height]);
+
+var svg = d3.select(".force-directed-graph")
+    .attr("width", width)
+    .attr("height", height);
+
+
+d3.json("/force-directed-graph/", function(error, graph) {
+    if (error) throw error;
+
+    var nodes = graph.nodes.slice(),
+        links = [],
+        bilinks = [];
+
+    graph.links.forEach(function(link) {
+        var s = nodes[link.source],
+            t = nodes[link.target],
+            i = {}; // intermediate node
+        nodes.push(i);
+        links.push({source: s, target: i}, {source: i, target: t});
+        bilinks.push([s, i, t]);
+    });
+
+    force
+        .nodes(nodes)
+        .links(links)
+        .start();
+
+    var link = svg.selectAll(".link")
+        .data(bilinks)
+        .enter().append("path")
+        .attr("class", "link");
+
+    var node = svg.selectAll(".node")
+        .data(graph.nodes)
+        .enter().append("circle")
+        .attr("class", "node")
+        .attr("r", 5)
+        .style("fill", function(d) { return color(d.group); })
+        .call(force.drag);
+
+    node.append("title")
+        .text(function(d) { return d.name; });
+
+    force.on("tick", function() {
+        link.attr("d", function(d) {
+            return "M" + d[0].x + "," + d[0].y
+                + "S" + d[1].x + "," + d[1].y
+                + " " + d[2].x + "," + d[2].y;
+        });
+        node.attr("transform", function(d) {
+            return "translate(" + d.x + "," + d.y + ")";
+        });
+    });
+});
