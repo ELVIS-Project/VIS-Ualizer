dd008e2d4ff7f037f71636ba317aa35c16fef0bd
AFFogarty <andrew.f.fogarty@gmail.com>
2016-03-29 13:11:47 -0400

    New: Allman Bar graph

--- server/static/js/bargraph.js
+++ server/static/js/bargraph.js
@@ -1,9 +1,13 @@
+var BarGraph = function(selector, width, height)
+{
+    var margin = {
+        top: 20,
+        right: 30,
+        bottom: 30,
+        left: 40
+    };
 
-
-var BarGraph = function(selector, width, height) {
-    var margin = {top: 20, right: 30, bottom: 30, left: 40};
-
-    // Which sortt we're using
+    // Which sort we're using
     var sort = {
         value: SortEnum.label,
         direction: SortDirectionEnum.ascending
@@ -26,35 +30,48 @@ var BarGraph = function(selector, width, height) {
         .scale(yScale)
         .orient("left");
 
-    var sortComparator = function(a,b) {
+    var sortComparator = function(a,b)
+    {
         var aValue,
             bValue;
 
-        if (sort.value === SortEnum.value) {
+        if (sort.value === SortEnum.value)
+        {
             aValue = a.value;
             bValue = b.value;
-        } else {
+        }
+        else
+        {
             aValue = a.label;
             bValue = b.label;
         }
 
-        if (sort.direction === SortDirectionEnum.ascending) {
+        if (sort.direction === SortDirectionEnum.ascending)
+        {
             return d3.ascending(aValue, bValue);
-        } else {
+        }
+        else
+        {
             return d3.descending(aValue, bValue);
         }
     };
     var colours = undefined;
 
-    function chart(data) {
+    function chart(data)
+    {
         // Back up the data
         chart.data = data;
         // Make sure the SVG is clean
         chart.g.selectAll("*").remove();
 
-        var labels = d3.map(data, function(d) { return d.label }).keys();
+        var labels = d3.map(data,
+            function(d)
+            {
+                return d.label
+            }).keys();
         // We only set colours the once
-        if (!colours) {
+        if (!colours)
+        {
             colours = d3.scale.category20().domain(labels);
         }
 
@@ -63,12 +80,18 @@ var BarGraph = function(selector, width, height) {
 
         // Map x-axis labels
         xScale.domain(
-            data.map(function(d) {
+            data.map(function(d)
+            {
                 return d.label;
-            })
-        );
+            }
+        ));
         // Map y-axis values
-        yScale.domain([0, d3.max(data, function(d) { return d.value; })]);
+        yScale.domain([0, d3.max(data,
+            function(d)
+            {
+                return d.value;
+            }
+        )]);
 
         // Draw the axes
         drawAxisLines(chart.g, xAxis, yAxis, computedHeight, 0, 0, 0);
@@ -78,10 +101,30 @@ var BarGraph = function(selector, width, height) {
             .selectAll(".bar")
             .data(data)
             .enter().append("rect")
-            .attr("x", function(d) { return xScale(d.label); })
-            .attr("y", function(d) { return yScale(d.value); })
-            .attr("fill", function(d) { return colours(d.label) })
-            .attr("height", function(d) { return computedHeight - yScale(d.value); })
+            .attr("x",
+                function(d)
+                {
+                    return xScale(d.label);
+                }
+            )
+            .attr("y",
+                function(d)
+                {
+                    return yScale(d.value);
+                }
+            )
+            .attr("fill",
+                function(d)
+                {
+                    return colours(d.label);
+                }
+            )
+            .attr("height",
+                function(d)
+                {
+                    return computedHeight - yScale(d.value);
+                }
+            )
             .attr("width", xScale.rangeBand())
             .style(cssStyling.bar);
     }
@@ -96,29 +139,48 @@ var BarGraph = function(selector, width, height) {
         .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    function attachSortChooser(selector) {
-        var parent = d3.select(selector).append("p").append("label")
+    function attachSortChooser(selector)
+    {
+        var parent = d3.select(selector)
+            .append("p")
+            .append("label")
             .text("Sort: ");
 
         var sortChooser = parent.append("select");
-        sortChooser.append("option").attr("value", "label").text("Label");
-        sortChooser.append("option").attr("value", "value").text("Value");
-        sortChooser.on("input", function() {
-            if (this.value === "label") {
+        sortChooser.append("option")
+            .attr("value", "label")
+            .text("Label");
+        sortChooser.append("option")
+            .attr("value", "value")
+            .text("Value");
+        sortChooser.on("input", function()
+        {
+            if (this.value === "label")
+            {
                 sort.value = SortEnum.label;
-            } else {
+            }
+            else
+            {
                 sort.value = SortEnum.value;
             }
             chart(chart.data);
         });
 
         var directionChooser = parent.append("select");
-        directionChooser.append("option").attr("value", "asc").text("Asc");
-        directionChooser.append("option").attr("value", "desc").text("Desc");
-        directionChooser.on("input", function() {
-            if (this.value === "asc") {
+        directionChooser.append("option")
+            .attr("value", "asc")
+            .text("Asc");
+        directionChooser.append("option")
+            .attr("value", "desc")
+            .text("Desc");
+        directionChooser.on("input", function()
+        {
+            if (this.value === "asc")
+            {
                 sort.direction = SortDirectionEnum.ascending;
-            } else {
+            }
+            else
+            {
                 sort.direction = SortDirectionEnum.descending;
             }
             chart(chart.data);
@@ -137,7 +199,8 @@ var BarGraph = function(selector, width, height) {
 /**
  * Load the bar graph data from the server, and render it if successful.
  */
-d3.json("/graph/", function(error, data) {
+d3.json("/graph/", function(error, data)
+{
     var barGraph = new BarGraph(".bar-graph", 640, 320);
     barGraph(data);
 });
